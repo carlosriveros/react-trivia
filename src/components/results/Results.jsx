bf6cf8d8a15c7469cf3e5785d54ac82aca94122e
carlos riveros <carlos.riveros@paytm.com>
2018-09-24 20:22:58 -0400

    fixed routing when clicking button from Results page

--- src/components/results/Results.jsx
+++ src/components/results/Results.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
-import { requestTriviaData, receiveTriviaData} from '../../actions/triviaActions'
 import { connect } from 'react-redux'
 import { fetch } from '../../services/http'
 import { List, Button } from 'semantic-ui-react'
 import HeadLine from '../../ui-components/HeadLine'
 import './results.css'
-
-const requestTrivia = () => (dispatch) => {
-  dispatch(requestTriviaData())
-  return fetch('https://opentdb.com/api.php?amount=10&difficulty=hard&type=boolean')
-  .then(response => {
-    dispatch(receiveTriviaData(response))
-  })
-}
 
 const mapState = ({ trivia}, ownProps) => {
   const correctAnswers = trivia.trivia.reduce((prev, curr) => {
@@ -22,13 +13,10 @@ const mapState = ({ trivia}, ownProps) => {
   return { trivia, correctAnswers, totalQuestions: trivia.trivia.length}
 }
 
-const Results = ({trivia: {trivia, requestingTrivia}, correctAnswers, totalQuestions,  requestTrivia, history}) => {
+const Results = ({trivia: {trivia, requestingTrivia}, correctAnswers, totalQuestions, history}) => {
 
-  const fetchTrivia = () => {
-    requestTrivia()
-      .then(response => {
-        history.push('1')
-      })
+  const playAgain = () => {
+    history.push('/')
   }
 
   if (!trivia.length) {
@@ -52,10 +40,10 @@ const Results = ({trivia: {trivia, requestingTrivia}, correctAnswers, totalQuest
       </List>
     </section>
     <footer>
-      <Button primary onClick={fetchTrivia} loading={requestingTrivia}>Play Again?</Button>
+      <Button primary onClick={playAgain}>Play Again?</Button>
     </footer>
   </React.Fragment>
   )
 }
 
-export default connect(mapState, {requestTrivia})(Results)
+export default connect(mapState, {})(Results)
